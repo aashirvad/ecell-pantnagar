95ce3b4e934c887c0f01231ecc467c5d7fa05284
meashirvad <aashirvadu@gamil.com>
2024-04-08 13:45:47 +0530

    initial :tada:

---
+++ README.md
@@ -0,0 +1,4 @@
+![quiz](https://user-images.githubusercontent.com/82800133/186756753-0c2d3178-abcc-4dac-9e18-fddf36a87bc4.png)
+
+
+E Cell Pantnagar
---
+++ script.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".blogs-slider", {
+  spaceBetween: 10,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+document.addEventListener("contextmenu", (event) => event.preventDefault());
